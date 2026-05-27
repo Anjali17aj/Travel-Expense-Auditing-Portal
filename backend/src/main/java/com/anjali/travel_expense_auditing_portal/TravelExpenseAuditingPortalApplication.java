82b4ffce64ab9a0958f4113c46c86a4086768dc9
Anjali17aj <sayhellotoanjali@gmail.com>
2026-05-27 23:23:27 +0530

    added Spring Boot backend project structure

---
+++ backend/src/main/java/com/anjali/travel_expense_auditing_portal/TravelExpenseAuditingPortalApplication.java
@@ -0,0 +1,13 @@
+package com.anjali.travel_expense_auditing_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelExpenseAuditingPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelExpenseAuditingPortalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/anjali/travel_expense_auditing_portal/TravelExpenseAuditingPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anjali.travel_expense_auditing_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelExpenseAuditingPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
